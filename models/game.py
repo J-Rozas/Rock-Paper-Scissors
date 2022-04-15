98948dc942cf0b64133f063835134d8d870d63a6
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-15 18:44:07 +0100

    Modify class game to use the class Player in the __init__

--- models/game.py
+++ models/game.py
@@ -2,5 +2,5 @@ from models.player import Player
 
 class Game:
     def __init__(self, input_player1, input_player2):
-        self.player1 = input_player1
-        self.player2 = input_player2
+        self.player1 = Player(input_player1)
+        self.player2 = Player(input_player2)

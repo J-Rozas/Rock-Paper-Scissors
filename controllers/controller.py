caf30ab4c35e4c66cd60a44f54da0fe4f04a4e80
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-15 22:49:37 +0100

    Update controller with new route result

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 from models.game import Game
 from models.player import Player
@@ -6,3 +6,16 @@ from models.player import Player
 @app.route("/")
 def home():
     return render_template("index.html.jinja", title = "Rock Paper Scissors")
+
+@app.route("/result", methods=["POST"])
+def round():
+    # Get info from the players that is collected from the form in the template index
+    player1 = Player(request.form["player1_name"], request.form["player1_choice"])
+    player2 = Player(request.form["player2_name"], request.form["player2_choice"])
+
+    # Create a new instance of the class Game with the information of the players and store it in the variable result_info    
+    # Play a round of the game
+    result_info = Game().play_round(player1, player2)
+
+    # Render a new html with the result of the round
+    return render_template("result.html.jinja", title = "Game result", result_info = result_info)

83de49459eb4cafe46d482ab96a11fda3bb11270
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-15 22:45:19 +0100

    Delete non-necessary import and add function with logic for the game

--- models/game.py
+++ models/game.py
@@ -1,7 +1,22 @@
-from models.player import Player
-
 class Game:
-    def play_round(self):
-        if self.player1.choice == "Rock":
-            if self.player2.choice == "Scissors":
-                return f"{self.player1.name} wins by playing Rock."
+    def play_round(self, player1, player2):
+        player1.choice = player1.choice.lower()
+        player2.choice = player2.choice.lower()
+
+        if player1.choice == player2:
+            return None
+        elif player1.choice == "rock":
+            if player2.choice == "paper":
+                return f"{player1.name} wins by playing Rock."
+            else:
+                return f"{player2.name} wins by playing Paper."
+        elif player1.choice == "paper":
+            if player2.choice == "rock":
+                return f"{player1.name} wins by playing Paper."
+            else:
+                return f"{player2.name} wins by playing Scissors."
+        else:
+            if player2.choice == "paper":
+                return f"{player1.name} wins by playing Scissors."
+            else:
+                return f"{player2.name} wins by playing Paper."
